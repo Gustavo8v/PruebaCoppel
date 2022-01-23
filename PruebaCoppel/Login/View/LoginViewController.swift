494e08ad6831bacc8bbc20489aa74fcf7732428d
Tavo <oliver.sykes.van@icloud.com>
2022-01-22 22:24:21 -0600

    inicializaciín de proyecto

---
+++ PruebaCoppel/Login/View/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  PruebaCoppel
+//
+//  Created by Gustavo on 22/01/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
